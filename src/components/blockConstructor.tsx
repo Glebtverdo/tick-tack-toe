29fd04074523196308a986f5240822ca8b26179f
unknown <glebtverd@gmail.com>
2022-09-27 22:30:50 +0300

    before eject

---
+++ src/components/blockConstructor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {circle, cross} from "@/utils/blockConstructor"
+
+function blockConstructor() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default blockConstructor;
